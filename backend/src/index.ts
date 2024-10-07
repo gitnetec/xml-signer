67f81cab91b0afcaca264fa3f07018d9c7955908
Leonardo <leonardo@ne.team>
2024-10-07 06:27:53 -0300

    chore: Update CORS configuration and Docker Compose file
    - Removed CORS configuration from backend code
    - Updated Docker Compose version to '3.7'
    - Added environment variables for frontend and backend services in Docker Compose
    - Set 'FORCE_COLOR' environment variable to "1" for both services
    - Added 'NEXT_PUBLIC_API_BASE_URL' environment variable for frontend service
    - Enabled tty mode for both services

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,6 @@ import signRoutes from './routes/signRoutes';
 
 const app = express();
 
-// Configuração do CORS
 app.use(cors({
   origin: 'https://xml.leonardogallo.co',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
@@ -14,7 +13,6 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 app.use('/output', express.static(path.join(__dirname, '..', 'output')));
 
